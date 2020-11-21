f76e38a9249e34e19241da80812eb994a91f02e3
Dmytrii Lysak <dlysak@live.com>
2020-11-21 23:44:35 +0200

    24

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -2,6 +2,7 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
 import { LessonService } from './lesson.service';
 import { CreateLessonInput } from './create-lesson.input';
+import { AssignStudentsToLessonInput } from './assign-students-to-lesson.type';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
@@ -22,5 +23,14 @@ export class LessonResolver {
     @Args('createLessonInput') createLessonInput: CreateLessonInput,
   ) {
     return this.lessonService.createLesson(createLessonInput);
+  }
+
+  @Mutation((returns) => LessonType)
+  assignStudentsToLesson(
+    @Args('assignStudentsToLessonInput')
+    assignStudentsToLessonInput: AssignStudentsToLessonInput,
+  ) {
+    const { lessonId, studentIds } = assignStudentsToLessonInput;
+    return this.lessonService.assignStudentsToLesson(lessonId, studentIds);
   }
 }
